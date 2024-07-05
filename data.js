a1c761707eaa51ad0664f1c4a3ce3043d00fc096
PriyanshuRaj213 <77734247+PriyanshuRaj213@users.noreply.github.com>
2024-07-05 17:47:30 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 let movies = [
     {
         name: 'falcon and the winter soldier',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'Following the events of 'Avengers: Endgame,'Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.',
         image: 'images/slider 2.PNG'
     },
     {
         name: 'loki',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”',
         image: 'images/slider 1.PNG'
     },
     {
         name: 'wanda vision',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.',
         image: 'images/slider 3.PNG'
     },
     {
         name: 'raya and the last dragon',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'In a realm known as Kumandra, a re-imagined Earth inhabited by an ancient civilization, a warrior named Raya is determined to find the last dragon.',
         image: 'images/slider 4.PNG'
     },
     {
         name: 'luca',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'On the Italian Riviera, an unlikely but strong friendship grows between a human being and a sea monster disguised as a human.',
         image: 'images/slider 5.PNG'
     }
 ]
